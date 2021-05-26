ac96db7784ad4bf6afc39f1997650be9bac22801
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2021-05-26 14:59:40 +0200

    Add button to activate the cropping and extend the demo application

---
+++ src/components/CroppingControls.js
@@ -0,0 +1,40 @@
+import CropIcon from '@material-ui/icons/Crop';
+import { MiradorMenuButton } from 'mirador/dist/es/src/components/MiradorMenuButton'
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const CroppingControls = ({
+  containerId,
+  options,
+  t,
+  updateOptions,
+}) => {
+  const {active, enabled} = options
+  if (!enabled) {
+    return null
+  }
+  return (
+    <MiradorMenuButton
+      aria-expanded={active}
+      aria-haspopup={true}
+      aria-label={active ? t('deactivateImageCropping') : t('activateImageCropping')}
+      color={active ? "primary" : ""}
+      containerId={containerId}
+      onClick={() => updateOptions({
+        ...options,
+        active: !active,
+      })}
+    >
+      <CropIcon />
+    </MiradorMenuButton>
+  )
+}
+
+CroppingControls.propTypes = {
+  containerId: PropTypes.string.isRequired,
+  options: PropTypes.object.isRequired,
+  t: PropTypes.func.isRequired,
+  updateOptions: PropTypes.func.isRequired,
+};
+
+export default CroppingControls
--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
-import React, {Component} from 'react'
+import { updateWindow } from 'mirador/dist/es/src/state/actions';
+import { getContainerId } from 'mirador/dist/es/src/state/selectors';
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+import CroppingControls from './components/CroppingControls'
+import {getWindowImageCropperOptions} from './state/selectors'
+
+export default [
+  {
+    component: CroppingControls,
+    mapDispatchToProps: (dispatch, { windowId }) => ({
+      updateOptions: (options) =>
+        dispatch(updateWindow(windowId, { imageCropper: options })),
+    }),
+    mapStateToProps: (state, { windowId }) => ({
+      containerId: getContainerId(state),
+      options: getWindowImageCropperOptions(state, {windowId}),
+    }),
+    mode: 'add',
+    target: 'WindowTopBarPluginArea',
   }
-}
+]
