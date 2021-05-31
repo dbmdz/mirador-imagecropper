c6dde2757f8d5ae3492f09577d790751db9f4864
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2021-05-31 14:11:40 +0200

    Fix linting and formatting issues

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,26 +1,40 @@
-import Mirador from 'mirador/dist/es/src/index';
+import Mirador from "mirador/dist/es/src/index";
 
-import imageCropperPlugin from '../../src';
+import imageCropperPlugin from "../../src";
 
 const config = {
   catalog: [
-    { manifestId: 'https://api.digitale-sammlungen.de/iiif/presentation/v2/bsb00135902/manifest', provider: 'Bavarian State Library' },
-    { manifestId: 'https://api.digitale-sammlungen.de/iiif/presentation/v2/bsb10532463_00005_u001/manifest', provider: 'Bavarian State Library' },
-    { manifestId: 'https://api.digitale-sammlungen.de/iiif/presentation/v2/bsb00034024/manifest', provider: 'Bavarian State Library' },
+    {
+      manifestId:
+        "https://api.digitale-sammlungen.de/iiif/presentation/v2/bsb00135902/manifest",
+      provider: "Bavarian State Library",
+    },
+    {
+      manifestId:
+        "https://api.digitale-sammlungen.de/iiif/presentation/v2/bsb10532463_00005_u001/manifest",
+      provider: "Bavarian State Library",
+    },
+    {
+      manifestId:
+        "https://api.digitale-sammlungen.de/iiif/presentation/v2/bsb00034024/manifest",
+      provider: "Bavarian State Library",
+    },
   ],
-  id: 'demo',
+  id: "demo",
   window: {
     imageCropper: {
+      active: true,
       enabled: true,
     },
   },
-  windows: [{
-    canvasIndex: 8,
-    manifestId: 'https://api.digitale-sammlungen.de/iiif/presentation/v2/bsb00034024/manifest',
-    view: 'single',
-  }],
+  windows: [
+    {
+      canvasIndex: 8,
+      manifestId:
+        "https://api.digitale-sammlungen.de/iiif/presentation/v2/bsb00034024/manifest",
+      view: "single",
+    },
+  ],
 };
 
-Mirador.viewer(config, [
-  ...imageCropperPlugin
-]);
+Mirador.viewer(config, [...imageCropperPlugin]);
