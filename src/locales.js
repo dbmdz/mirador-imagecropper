2b3ebd527a23753974d064345e7562a9ab1263ae
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2021-07-16 14:51:58 +0200

    Add translations

---
+++ src/locales.js
@@ -0,0 +1,14 @@
+export default {
+  de: {
+    imageCropper: {
+      activate: "Auswahl eines Bildausschnitts aktivieren",
+      deactivate: "Auswahl eines Bildausschnitts deaktivieren",
+    },
+  },
+  en: {
+    imageCropper: {
+      activate: "Activate selection of an image region",
+      deactivate: "Deactivate selection of an image region",
+    },
+  },
+};
