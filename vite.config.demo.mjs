26f88dce7bf661b9332119939aa18193279d3694
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2026-08-14 10:50:01 +0200

    Refactor Vite configuration

---
+++ vite.config.demo.mjs
@@ -0,0 +1,18 @@
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    outDir: resolve(import.meta.dirname, "demo/dist"),
+    rollupOptions: {
+      input: {
+        demo: resolve(import.meta.dirname, "index.html"),
+      },
+    },
+  },
+  plugins: [react()],
+  server: {
+    open: true,
+  },
+});
